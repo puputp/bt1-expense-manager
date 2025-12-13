54f73c659f8fb37feb9bc2dcd7a091cbb65a5864
Thien Phuc <thijenphuc@gmail.com>
2025-12-13 23:12:22 +0700

    update frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const API = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: `${API}/api`,
   headers: { "Content-Type": "application/json" },
 });
